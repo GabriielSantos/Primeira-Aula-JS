5cb056653dbefe07a060dd58e697063664db7a7f
Gabriel Santos <gabrielcsantos.dev@gmail.com>
2022-06-06 21:05:43 -0300

    06-06-22

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+var nome = "Gabriel Santos";
+var n1 = 26;
+var n2 = 10;
+var frase = "Japão Pentacampeão";
+//alert (nome + " tem " + idade + " anos ");
+alert (n1 + n2);
+console.log (nome);
+console.log (n1 * n2);
+console.log (frase.toUpperCase("Japão", "Brasil"));
+  
